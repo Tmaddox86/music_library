cbc93ec176550ad8c34bf7814ee54fd144f5525b
Tara Maddox <TMaddox85@gmail.com>
2023-03-19 16:15:42 -0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
 import  {useEffect, useState, useRef } from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Gallery from './components/Gallery.js';
 import SearchBar from './components/SearchBar.js';
 import {DataContext} from './Context/DataContext.js';
 import {SearchContext} from './Context/SearchContext.js'
+import AlbumView from './components/AlbumView.js';
+import ArtistView from './components/ArtistView.js';
 
 function App() {
-  let [search, setSearch] = useState ('the greatful dead')
+  
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
   let [numberRef] = useRef(1);
-  //let [darkMode, setDarkMode] = useState(true)
   let searchInput = useRef('')
-console.log(typeof numberRef)
 
-  useEffect (() => {
-    if (search) {
-
-    }
-  }, [search])
-  
+  const API_URL = 'https://itunes.apple.com/search?term='
 
   const handleSearch = (e, term) => {
     e.preventDefault()
-    setSearch()
-    fetch ('https://itunes.apple.com/search?term=the%20grateful%20dead')
-      .then(response => response.json())
-      .then(({ resultCount }) => {
-        const successMessage = `Successfully fetched ${resultCount} resultCount(s)!`
-        const errorMessage = "Not Found"
-        setMessage(resultCount ? successMessage : errorMessage)
-        setData(song)
-        console.log('The number of calls that we have made is' + numberRef.current++)
-  }, fetch() )
+    const fetchData = async () => {
+			document.title = `${term} Music`
+			const response = await fetch(API_URL + term)
+			const resData = await response.json()
+			if (resData.results.length > 0) {
+				// Set State and Context value
+				return setData(resData.results)
+			} else {
+				return setMessage('Not Found')
+			}
+		}
+		fetchData()
+	}
 
   return (
     <div className ='App'>
       <SearchContext.Provider value={{
-        ref: searchInput, handleSearch: handleSearch
+        term: searchInput, handleSearch: handleSearch
       }}> 
-      <SearchBar handleSearch={setSearch}/> 
+      <SearchBar handleSearch={handleSearch}/> 
       </SearchContext.Provider>
       {message}
       <DataContext.Provider value = {[data]}>
-      <Gallery data = {data}/>
+      <Gallery/>
       </DataContext.Provider>
     </div>
   )
-}}
+}
 
 export default App;
