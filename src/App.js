a636baac12049e7eced1d43a04fe6c4b4c5169e6
Tara Maddox <TMaddox85@gmail.com>
2023-03-22 18:10:22 -0700

    all tech

--- src/App.js
+++ src/App.js
@@ -31,34 +31,17 @@ function App() {
 		}
 		fetchData()
 	}
-const API_URL = 'https://itunes.apple.com/search?term='
-  
-  const handleSearch = (e, term) => {
-    e.preventDefault()
-    
-    const fetchData = async () => {
-      document.title = `${search} Music`
-      const response = await fetch(API_URL + search)
-      const resData = await response.json()
-      if (resData.results.length > 0) {
-        setData(resData.results)
-      } else {
-        setMessage('Not Found')
-      }
-    }
-    fetchData()
-  } 
 
   return (
     <div className ='App'>
       <SearchContext.Provider value={{
         term: searchInput, handleSearch: handleSearch
-      }}> 
+      }}>
       <SearchBar handleSearch={handleSearch}/> 
       <SearchBar/> 
       </SearchContext.Provider>
       {message}
-      <DataContext.Provider value = {[data]}>
+      <DataContext.Provider value = {(data)}>
       <Gallery/>
       </DataContext.Provider>
     </div>
