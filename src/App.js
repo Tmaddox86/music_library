d8ce212b2a16b0248fdc08574641e664b3ce939c
Tara Maddox <TMaddox85@gmail.com>
2023-03-23 14:10:05 -0700

    combine music library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import  {useEffect, useState, useRef } from 'react';
+import  {useEffect, useState, Suspense } from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Gallery from './components/Gallery.js';
 import SearchBar from './components/SearchBar.js';
@@ -6,15 +6,24 @@ import {DataContext} from './Context/DataContext.js';
 import {SearchContext} from './Context/SearchContext.js'
 import AlbumView from './components/AlbumView.js';
 import ArtistView from './components/ArtistView.js';
+import { createResource as fetchData } from './helper'
+
 
 function App() {
   
   let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
+  let [data, setData] = useState([null])
   let [numberRef] = useRef(1);
   let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
+
+  useEffect(() => {
+    if (searchTerm) {
+        setData(fetchData(searchTerm))
+    }
+}, [searchTerm])
+
 
   const handleSearch = (e, term) => {
     e.preventDefault()
@@ -32,19 +41,27 @@ function App() {
 		fetchData()
 	}
 
+
+}
+
+const renderGallery =() => {
+  if (data) {
   return (
-    <div className ='App'>
-      <SearchContext.Provider value={{
-        term: searchInput, handleSearch: handleSearch
-      }}> 
-      <SearchBar handleSearch={handleSearch}/> 
-      </SearchContext.Provider>
-      {message}
-      <DataContext.Provider value = {[data]}>
-      <Gallery/>
-      </DataContext.Provider>
+    <div>
+      <Suspense fallback={<h1><Spinner/></h1>}>
+        <Gallery data={data}/>
+      </Suspense>
     </div>
   )
+  }
+return (
+ <div className="App">
+  <SearchBar handleSearch={handleSearch}/>
+  {message}
+  {renderGallery()}
+</div>
+)
+
 }
 
 export default App;
